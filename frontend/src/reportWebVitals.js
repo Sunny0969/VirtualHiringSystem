035c413a87a4e2a7d311ee3fcaba711cbeaee788
Sohaib-Hassan <69350168+Sohaib-Hassan@users.noreply.github.com>
2023-11-27 12:14:24 +0500

    Refactor reportWebVitals for async/await and Promise.all usage
    Update reportWebVitals.js

--- frontend/src/reportWebVitals.js
+++ frontend/src/reportWebVitals.js
@@ -1,12 +1,17 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+const reportWebVitals = async (onPerfEntry) => {
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    try {
+      const { getCLS, getFID, getFCP, getLCP, getTTFB } = await import('web-vitals');
+      await Promise.all([
+        getCLS(onPerfEntry),
+        getFID(onPerfEntry),
+        getFCP(onPerfEntry),
+        getLCP(onPerfEntry),
+        getTTFB(onPerfEntry)
+      ]);
+    } catch (error) {
+      console.error('Error loading web-vitals:', error);
+    }
   }
 };
 
